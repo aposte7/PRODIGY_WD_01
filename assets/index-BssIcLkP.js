61ecd71e83b80a25f075850f542bd2a71030d36f
aposte7 <olman4jesus@gmail.com>
2025-03-01 23:43:38 +0300

    Updates

--- assets/index-BssIcLkP.js
+++ assets/index-BssIcLkP.js
@@ -1,4 +1,4 @@
-(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const t of s)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&o(r)}).observe(document,{childList:!0,subtree:!0});function a(s){const t={};return s.integrity&&(t.integrity=s.integrity),s.referrerPolicy&&(t.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?t.credentials="include":s.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(s){if(s.ep)return;s.ep=!0;const t=a(s);fetch(s.href,t)}})();function n(e,i,a="innerHTML"){const o=typeof e=="string"?document.querySelector(e):e,s={innerHTML:()=>o.innerHTML=i,append:()=>o.insertAdjacentHTML("beforeend",i),prepend:()=>o.insertAdjacentHTML("afterbegin",i),before:()=>o.insertAdjacentHTML("beforebegin",i),after:()=>o.insertAdjacentHTML("afterend",i)};let t={status:"not-started",message:""};if(!o)return{...t,status:"error",message:"Parent element not found"};try{if(!s[a])throw new Error("Invalid method specified");return s[a](),{...t,status:"completed",message:`HTML inserted successfully using method: ${a}`}}catch(r){return console.log(`error occurred while adding children to ${e}`),{...t,status:"error",message:r.message||"Failed to insert HTML"}}}function l(e){n(e,`    
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const i of s)if(i.type==="childList")for(const n of i.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(s){const i={};return s.integrity&&(i.integrity=s.integrity),s.referrerPolicy&&(i.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?i.credentials="include":s.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(s){if(s.ep)return;s.ep=!0;const i=o(s);fetch(s.href,i)}})();function r(e,t,o="innerHTML"){const a=typeof e=="string"?document.querySelector(e):e,s={innerHTML:()=>a.innerHTML=t,append:()=>a.insertAdjacentHTML("beforeend",t),prepend:()=>a.insertAdjacentHTML("afterbegin",t),before:()=>a.insertAdjacentHTML("beforebegin",t),after:()=>a.insertAdjacentHTML("afterend",t)};let i={status:"not-started",message:""};if(!a)return{...i,status:"error",message:"Parent element not found"};try{if(!s[o])throw new Error("Invalid method specified");return s[o](),{...i,status:"completed",message:`HTML inserted successfully using method: ${o}`}}catch(n){return console.log(`error occurred while adding children to ${e}`),{...i,status:"error",message:n.message||"Failed to insert HTML"}}}function c(e){r(e,`    
     <div>
         <div class="footer-content">
             <div class="footer-section">
@@ -47,18 +47,18 @@
     </div>
 </div>
 
-    `)}function d(e){const i=` 
+    `)}function u(e){const t=` 
 		<div class="nav_list">
-			${c("Home")}
-			${c("Solution")}
-			${c("Resource")}
-			${c("About")}
-		</div>`;n(e,i),document.addEventListener("DOMContentLoaded",()=>{const a=document.querySelectorAll(".nav_item");a.forEach(o=>{o.addEventListener("click",function(){a.forEach(s=>s.classList.remove("nav_active")),this.classList.add("nav_active")})})})}function c(e,i="#"){return`
+			${l("Home")}
+			${l("Solution")}
+			${l("Resource")}
+			${l("About")}
+		</div>`;r(e,t),document.addEventListener("DOMContentLoaded",()=>{const o=document.querySelectorAll(".nav_item");o.forEach(a=>{a.addEventListener("click",function(){o.forEach(s=>s.classList.remove("nav_active")),this.classList.add("nav_active")})})})}function l(e,t="#"){return`
 	<div class="nav_item">
-		<a class="nav_link" href=${i}>
+		<a class="nav_link" href=${t}>
 			${e}
 		</a>
-	</div>`}function u(e){const a=4.615384615384615,o=`
+	</div>`}function d(e){const o=4.615384615384615,a=`
         <div class="nav_bar">
             <a href="#" class="nav_logo">
                 <img  src="assets/icons/logo.svg" alt="Logo">
@@ -66,7 +66,12 @@
 
             <div id="nav_container"></div>
             
-            <a href="#" class="nav_link-btn">Sign In</a>
+            <div class="nav_action">
+                <a href="#" class="nav_link-btn btn ">Sign In</a>
+                <button class="menu_btn" data-toggle-menu>
+                    <span class="menu_bar"></span>
+                </button>
+            </div>
         </div>
 
         <div class="hero_wrapper">
@@ -98,18 +103,18 @@
             <div class="supporter_wrapper">
             <p class="supporter_text">Specified by Love</p>
             
-            ${Array.from({length:13},(s,t)=>`
-                    <div style="animation-delay: -${a*t+1}s" 
+            ${Array.from({length:13},(s,i)=>`
+                    <div style="animation-delay: -${o*i+1}s" 
                     
-                    data-image-index="${t+1}" 
+                    data-image-index="${i+1}" 
                     class="supporter_img-container">
-                        <img class="supporter_img" src="assets/icons/s-${t+1}.svg" alt="" />
+                        <img class="supporter_img" src="assets/icons/s-${i+1}.svg" alt="" />
                     </div>
                 `).join("")}
 
             </div>
         </div>
-`;n(e,o),d("#nav_container")}function p(e){n(e,`
+`;r(e,a),u("#nav_container")}function p(e){r(e,`
 	<div class="service_content">
 		<div class="service_text">
             <h2 class="service_text-header">
@@ -150,23 +155,116 @@
                     <img src="assets/images/service_box_1.jpg" alt="" />
                 </div>
             </div>
+
             <div class="service_box">
                 <div class="service_box-wrapper">
                     <h2 class="service_box-header">
-                    Sync your tokens
+                    Customize outputs
                     </h2>
 
                     <p class="service_box-description">
-                        Specify is natively compatible with Figma, GitHub, Notion, Raycast, and many more to come. Our REST API and CLI help you to connect even more. 
+                        Specify’s open source parsers help you generate design tokens and assets that match your company standards. 
                     </p>
 
                     <div class="service_btn">
-                        <a href="#" class="btn btn-link">Discover all available apps -> </a>
+                        <a href="#" class="btn btn-link">Discover all parsers -> </a>
                     </div>
                 </div>
 
-                <div class="service_image">
-                    <img src="assets/images/service_box_1.jpg" alt="" />
+                <div class="service_image service_image-2 flex">
+                    <ul style="--t:30s" class="service_tag flex">
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_1.svg" alt="" />
+                            <p>to-css-custom-properties</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_3.svg" alt="" />
+                            <p>replace-string</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_1.svg" alt="" />
+                            <p>to-css-custom-properties</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_3.svg" alt="" />
+                            <p>replace-string</p>
+                        </li>
+
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                    </ul>
+                    <ul style="--t:20s" class="service_tag flex">
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_1.svg" alt="" />
+                            <p>to-css-custom-properties</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_3.svg" alt="" />
+                            <p>replace-string</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_1.svg" alt="" />
+                            <p>to-css-custom-properties</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_3.svg" alt="" />
+                            <p>replace-string</p>
+                        </li>
+
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                    </ul>
+                    <ul style="--t:20s" class="service_tag flex">
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_1.svg" alt="" />
+                            <p>to-css-custom-properties</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_3.svg" alt="" />
+                            <p>replace-string</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_1.svg" alt="" />
+                            <p>to-css-custom-properties</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_3.svg" alt="" />
+                            <p>replace-string</p>
+                        </li>
+
+                        <li class="flex service_tag_item">
+                            <img height="5" width="5" src="assets/outputs/out_2.svg" alt="" />
+                            <p>to-flutter</p>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -211,7 +309,7 @@
         </div>
 
 	</div>
-		`)}function v(e){n(e,`
+		`)}function v(e){r(e,`
 	<div class="showcase_content">
 		<div class="showcase_text">
             <h2 class="showcase_text-header">
@@ -270,7 +368,7 @@
 			</div>
 		</div>
 	</div>
-		`)}function m(e){const a=`
+		`)}function g(e){const o=`
     <div class="testimony_content">
         <div class="testimony_text">
         <h2 class="testimony_text-header">Trusted by product teams</h2>
@@ -285,17 +383,17 @@
         </div>
         <div class="testimony_wrapper">
            <div class="testimony_box-list">
-		 		 ${[{name:"Roy Vab Rooijen",role:"Graphics Designer",imgUrl:"assets/users/u_1.jpg",message:"The automation from design to code with Specify is incredibly powerful. The ability to reference the same tokens and assets in Figma — and in our codebase — saves us an incredible amount of time, while reducing manual, error-prone work."},{name:"Jane Doe",role:"UI/UX Designer",imgUrl:"assets/users/u_2.jpg",message:"Specify is the missing link between our design and engineering teams. It definitely helped us integrate design tokens in an automated way very quickly and seamlessly into our codebase."},{name:"John Smith",role:"Frontend Developer",imgUrl:"assets/users/u_3.jpg",message:"We sync our design tokens and icons to our tools with Specify, and it gives our design and engineering teams peace of mind!"},{name:"Alice Johnson",role:"Product Manager",imgUrl:"assets/users/u_4.jpg",message:"The automation from design to code with Specify is incredibly powerful. The ability to reference the same tokens and assets in Figma — and in our codebase — saves us an incredible amount of time, while reducing manual, error-prone work."},{name:"Bob Brown",role:"Backend Developer",imgUrl:"assets/users/u_5.jpg",message:"Specify helps our designers keep their assets and design decisions in sync with the actual code, avoiding extra development effort and making design implementation much more flexible."},{name:"Charlie Davis",role:"Full Stack Developer",imgUrl:"assets/users/u_3.jpg",message:"Specify is the perfect addition to our front-end boilerplate. Tokens, assets, fonts: everything comes straight out from Figma. Less configuration, more creative development. Our team loves it!"}].map((o,s)=>g(o,s)).join("")}  
+		 		 ${[{name:"Roy Vab Rooijen",role:"Graphics Designer",imgUrl:"assets/users/u_1.jpg",message:"The automation from design to code with Specify is incredibly powerful. The ability to reference the same tokens and assets in Figma — and in our codebase — saves us an incredible amount of time, while reducing manual, error-prone work."},{name:"Jane Doe",role:"UI/UX Designer",imgUrl:"assets/users/u_2.jpg",message:"Specify is the missing link between our design and engineering teams. It definitely helped us integrate design tokens in an automated way very quickly and seamlessly into our codebase."},{name:"John Smith",role:"Frontend Developer",imgUrl:"assets/users/u_3.jpg",message:"We sync our design tokens and icons to our tools with Specify, and it gives our design and engineering teams peace of mind!"},{name:"Alice Johnson",role:"Product Manager",imgUrl:"assets/users/u_4.jpg",message:"The automation from design to code with Specify is incredibly powerful. The ability to reference the same tokens and assets in Figma — and in our codebase — saves us an incredible amount of time, while reducing manual, error-prone work."},{name:"Bob Brown",role:"Backend Developer",imgUrl:"assets/users/u_5.jpg",message:"Specify helps our designers keep their assets and design decisions in sync with the actual code, avoiding extra development effort and making design implementation much more flexible."},{name:"Charlie Davis",role:"Full Stack Developer",imgUrl:"assets/users/u_3.jpg",message:"Specify is the perfect addition to our front-end boilerplate. Tokens, assets, fonts: everything comes straight out from Figma. Less configuration, more creative development. Our team loves it!"}].map((a,s)=>m(a,s)).join("")}  
 		   </div>
 			  
         </div>
     </div>
-    `;n(e,a)}function g(e,i){return`
-        <div style="animation-delay: -${10 .toFixed(0)*i+1}s"  class="testimony_box">
+    `;r(e,o)}function m(e,t){return`
+        <div style="animation-delay: -${10 .toFixed(0)*t+1}s"  class="testimony_box">
             
 			<div class="testimony_logo">
                 <div class="logo">
-					<img src="assets/icons/s-${i+1}.svg" alt="companies logo" />
+					<img src="assets/icons/s-${t+1}.svg" alt="companies logo" />
 				</div>
             </div>
 
@@ -314,7 +412,7 @@
                 
             </div>
          </div>
-    `}function h(e){n(e,`
+    `}function h(e){r(e,`
     <div>
         <header class="header"></header>
 
@@ -328,8 +426,8 @@
 
     </div>
 
-  `),u(".header"),v(".showcase"),p(".service"),m(".testimony"),l(".footer")}document.querySelector("#app").innerHTML=`
+  `),d(".header"),v(".showcase"),p(".service"),g(".testimony"),c(".footer")}document.querySelector("#app").innerHTML=`
   <div id="page">
     
   </div>
-`;h("#page");
+`;h("#page");const f=()=>{document.querySelector("[data-toggle-menu]").addEventListener("click",()=>{document.querySelector("#nav_container").classList.toggle("active"),document.querySelector(".menu_btn").classList.toggle("active")})};f();
